39653ea80407ca45dda44b20d65d2e63adbbb650
AntonRange <00antran@skola.boras.se>
2023-06-05 17:31:07 +0200

    quotes fetch

---
+++ index.js
@@ -0,0 +1,11 @@
+function fetchQueotes() {
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
+}
+
+module.export = { fetchQueotes }
