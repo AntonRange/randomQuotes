6356f4f1b656b68d50d43cf25a624cd80f235c9e
AntonRange <00antran@skola.boras.se>
2023-06-05 18:16:15 +0200

    små ändringar

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-function fetchQueotes() {
-fetch("https://type.fit/api/quotes")
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  });
+function fetchQuotes() {
+  return fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      return data;
+    });
 }
 
-module.export = { fetchQueotes }
+module.exports = { fetchQuotes };
